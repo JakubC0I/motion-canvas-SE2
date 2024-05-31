7d16bf64ed03f8471175cf49cce45a6fed5efb49
Implomin <Implomin@gmail.com>
2024-05-31 10:46:16 +0200

    test: init gif (copy of image)

--- packages/template/src/scenes/example.tsx
+++ packages/template/src/scenes/example.tsx
@@ -1,4 +1,4 @@
-import {Rect, makeScene2D} from '@motion-canvas/2d';
+import {Img, Rect, makeScene2D} from '@motion-canvas/2d';
 import {
   all,
   createRef,
@@ -7,6 +7,7 @@ import {
   waitFor,
   waitUntil,
 } from '@motion-canvas/core';
+import examplePng from './gif.gif';
 
 export default makeScene2D(function* (view) {
   const rect = createRef<Rect>();
@@ -15,9 +16,17 @@ export default makeScene2D(function* (view) {
     <Rect ref={rect} size={320} radius={80} smoothCorners fill={'#f3303f'} />,
   );
 
-  yield* waitUntil('rect');
+  const imageRef = createRef<Img>();
+  view.add(<Img ref={imageRef} src={examplePng} scale={2} />);
+
+  /*   yield* waitUntil('rect');
   yield* rect().scale(2, 1, easeInOutExpo).to(1, 0.6, easeInExpo);
   rect().fill('#ffa56d');
   yield* all(rect().ripple(1));
+  yield* waitFor(0.3); */
+
+  yield* waitUntil('imageRef');
+  yield* imageRef().scale(2, 1, easeInOutExpo).to(1, 0.6, easeInExpo);
+  yield* all(imageRef().ripple(1));
   yield* waitFor(0.3);
 });
